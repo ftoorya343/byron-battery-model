9d02773cf074bf0c417ae538153930a94e46e840
lukasmarshall <lukasmarshall@gmail.com>
2017-07-02 15:11:15 +0930

    2016 prices in there.

--- simulate-live.py
+++ simulate-live.py
@@ -10,6 +10,7 @@ from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.plotting import figure
 from bokeh.models.widgets import Button
+from bokeh.models.widgets import Select
 
 
 
@@ -58,11 +59,12 @@ lines = list(lines)
 for line in lines:
 	line['All'] = float(line['All'])
 	line['Optimal'] = float(line['Optimal'])
-	line['Price'] = float(line['Price'])
+	line['Price-2015'] = float(line['Price-2015'])
+	line['Price-2016'] = float(line['Price-2016'])
 	
 
 
-def calculateBatteryRevenue(lines, MWh, priceThreshold, solarCapacityMW):
+def calculateBatteryRevenue(year, lines, MWh, priceThreshold, solarCapacityMW):
 	batteryRevenue = 0
 	allRevenue = 0
 	optimalRevenue = 0
@@ -71,8 +73,8 @@ def calculateBatteryRevenue(lines, MWh, priceThreshold, solarCapacityMW):
 
 	# Pure solar NEM revenue
 	for line in lines:
-		line['pure-solar-revenue-all'] = line['All'] * solarCapacityMW * line['Price']
-		line['pure-solar-revenue-optimal'] = line['Optimal'] * solarCapacityMW * line['Price']
+		line['pure-solar-revenue-all'] = line['All'] * solarCapacityMW * line['Price-'+year]
+		line['pure-solar-revenue-optimal'] = line['Optimal'] * solarCapacityMW * line['Price-'+year]
 		allRevenue  +=line['pure-solar-revenue-all']
 		optimalRevenue  +=line['pure-solar-revenue-optimal']
 		totalEnergy += line['Optimal'] * solarCapacityMW
@@ -85,14 +87,14 @@ def calculateBatteryRevenue(lines, MWh, priceThreshold, solarCapacityMW):
 	battery = Battery(MWh)
 	for dataPoint in lines:
 		# If under threshold......
-		if(dataPoint['Price'] < priceThreshold):
+		if(dataPoint['Price-'+year] < priceThreshold):
 			# Charge the battery, get whatever is left over. 
 			remainingMWh = battery.charge(dataPoint['Optimal'] * solarCapacityMW)
 			# Sell the remaining energy
-			dataPoint['battery-revenue'] = remainingMWh * dataPoint['Price']
+			dataPoint['battery-revenue'] = remainingMWh * dataPoint['Price-'+year]
 		else:
 			# Sell whatever we can from the battery and the solar system.
-			dataPoint['battery-revenue'] = dataPoint['Price'] * (dataPoint['Optimal'] * solarCapacityMW + battery.discharge())
+			dataPoint['battery-revenue'] = dataPoint['Price-'+year] * (dataPoint['Optimal'] * solarCapacityMW + battery.discharge())
 		batteryRevenue  +=dataPoint['battery-revenue']
 			
 	print "Finished Calculating Battery Revenue"
@@ -138,7 +140,7 @@ def generateChartingDict(lines):
 	return source
 
 
-(numCycles, allRevenue, optimalRevenue, batteryRevenue, totalEnergy) = calculateBatteryRevenue(lines=lines, MWh=0.099, priceThreshold=80, solarCapacityMW = 0.099)
+(numCycles, allRevenue, optimalRevenue, batteryRevenue, totalEnergy) = calculateBatteryRevenue(year="2015",lines=lines, MWh=0.099, priceThreshold=80, solarCapacityMW = 0.099)
 
 source = ColumnDataSource(data=generateChartingDict(lines))
 
@@ -153,7 +155,7 @@ output_file("lines.html")
 
 
 # Set up the plot
-plot = figure(title="Cycles: "+str("%.2f" %numCycles)+" PV Revenue: $"+str("%.2f" %allRevenue)+" Optimal PV Revenue: $"+str("%.2f" %optimalRevenue)+" Battery Revenue: $"+str("%.2f"%batteryRevenue)+"          (Optimal PV $"+str("%.2f" %(optimalRevenue/totalEnergy))+"/MWh)   (Battery $"+str("%.2f" %(batteryRevenue/totalEnergy))+"/MWh)", 
+plot = figure(title="2015 Cycles: "+str("%.2f" %numCycles)+" PV Revenue: $"+str("%.2f" %allRevenue)+" Optimal PV Revenue: $"+str("%.2f" %optimalRevenue)+" Battery Revenue: $"+str("%.2f"%batteryRevenue)+"          (Optimal PV $"+str("%.2f" %(optimalRevenue/totalEnergy))+"/MWh)   (Battery $"+str("%.2f" %(batteryRevenue/totalEnergy))+"/MWh)", 
 	x_axis_label='Hour of Year', 
 	y_axis_label='Revenue ($AUD)', 
 	width=1200)
@@ -170,15 +172,22 @@ batterySizeMWh = Slider(title="Battery Capacity (MWh)", value=0.099, start=0, en
 solarSizeMWh = Slider(title="Solar Capacity (MW)", value=0.099, start=0, end=2.0, step=0.001)
 priceThreshold = Slider(title="Battery Dispatch Price Threshold ($/MWh)", value=80.0, start=80.0, end=1000.0)
 
+
+def radioGroup_handler(new):
+	print "New year selected: "+str(new)
+
+select = Select(title="Year", value="2015", options=["2015", "2016"])
+
+
 def update_data():
 	# Get the current slider values
 	solarCap = solarSizeMWh.value
 	batteryCap = batterySizeMWh.value
 	priceThresh = priceThreshold.value
 
-	(numCycles, allRevenue, optimalRevenue, batteryRevenue, totalEnergy) = calculateBatteryRevenue(lines=lines, MWh=batteryCap, priceThreshold=priceThresh, solarCapacityMW = solarCap)
+	(numCycles, allRevenue, optimalRevenue, batteryRevenue, totalEnergy) = calculateBatteryRevenue(year= select.value, lines=lines, MWh=batteryCap, priceThreshold=priceThresh, solarCapacityMW = solarCap)
 	source.data = generateChartingDict(lines)
-	titleText = "Cycles: "+str("%.2f" %numCycles)+" PV Revenue: $"+str("%.2f" %allRevenue)+" Optimal PV Revenue: $"+str("%.2f" %optimalRevenue)+" Battery Revenue: $"+str("%.2f"%batteryRevenue)+"          (Optimal PV $"+str("%.2f" %(optimalRevenue/totalEnergy))+"/MWh)   (Battery $"+str("%.2f" %(batteryRevenue/totalEnergy))+"/MWh)"
+	titleText = str(select.value)+" Cycles: "+str("%.2f" %numCycles)+" PV Revenue: $"+str("%.2f" %allRevenue)+" Optimal PV Revenue: $"+str("%.2f" %optimalRevenue)+" Battery Revenue: $"+str("%.2f"%batteryRevenue)+"          (Optimal PV $"+str("%.2f" %(optimalRevenue/totalEnergy))+"/MWh)   (Battery $"+str("%.2f" %(batteryRevenue/totalEnergy))+"/MWh)"
 	# titleText += 
 	plot.title.text =titleText
 # for w in [batterySizeMWh, solarSizeMWh, priceThreshold]:
@@ -191,7 +200,7 @@ button = Button(label="Re-Calculate", button_type="success")
 button.on_click(update_data)
 
 
-inputs = widgetbox(batterySizeMWh, solarSizeMWh, priceThreshold, button)
+inputs = widgetbox(select, batterySizeMWh, solarSizeMWh, priceThreshold, button)
 
 curdoc().add_root(row(inputs, plot, width=800))
 curdoc().title = "Solar and Battery Simulator"
